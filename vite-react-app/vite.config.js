8219db76f9295d6c374d5390e94a476091dae968
omarabdulrahman99 <saintnicks78@hotmail.com>
2024-03-01 09:32:04 -0500

    onrender commit

--- vite-react-app/vite.config.js
+++ vite-react-app/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: "http://localhost:5000",
+        target: "https://tweetnest.onrender.com",
         changeOrigin: true,
         secure: false,
       },
